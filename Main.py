de344bf133c49e073a1e7a52c5d6e3f3c541efd4
JustAnotherCoder <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-05-08 11:47:02 +0100

    Fixed a future

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 import time
-print("You have found a strange looking house")
+print("You are a adventurer strolling throu the forrest")
 time.sleep(1)
 input("press space to continue")
+print("You have found a strange looking house")
+input()
 if input("go in to the house?(type 1 for yes type 2 for no)")==2:
   print("you walk away and go about your life.")
   time.sleep(1)
   print("THE END.")
+  exit()
+
