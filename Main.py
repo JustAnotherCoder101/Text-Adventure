5fa1474cec5ea28ea68e07300ee4e256e9cbf3cd
JustAnotherCoder <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-05-08 13:02:35 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import time
+import random
+goldCoins=10
 print("you are a brave adventurer looking for a quest")
 time.sleep(1)
 input("press enter to continue")
@@ -12,6 +14,11 @@ time.sleep(1)
 thing = input("turn left or right?(type 1 for right type 2 for left)")
 if thing == 1:
   print("You have reached a door")
+  input("You go in the door")
+  print("You find a chest with 30 gold coins! ")
+  input("+30 gold coins")
+  goldCoins+=30
+  print("your total is:"+goldCoins)
 elif thing == 2:
   print("You have found a trapdoor")
   
