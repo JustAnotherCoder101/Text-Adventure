dfe08d89607c716ce722469ec12fa88b05cc8795
JustAnotherCoder <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-05-08 12:09:25 +0100

    Fixed bugs

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import time
 print("you are a brave adventurer looking for a quest")
 time.sleep(1)
-input("press space to continue")
+input("press enter to continue")
 print("you spot a Castle")
 input()
 print("you have decided to see what awaits in this castle")
@@ -9,7 +9,7 @@ input
 print("you have entered the castle")
 time.sleep(1)
 
-thing
+thing = input("turn left or right?(type 1 for right type 2 for left)")
 if thing == 1:
   print("You have reached a door")
 elif thing == 2:
