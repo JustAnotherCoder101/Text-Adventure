1129c16b098b54b625ee1948350f9423f0515007
JustAnotherCoder101 <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-05-30 12:22:25 +0000

    Added to tutorial

--- function.py
+++ function.py
@@ -1,10 +1,14 @@
 def tshop(Coins):
   print("You reach the blacksmith's")
   print("BlackSmith:Sorry Kid,this sword is the only thing available today")
+  print("You Great!")
   input()
   while True:
     if input("press 1 to buy the sword for 5 coins."):
+      Coins =- 5
       print("Sword bought!")
       print("you now have "+str(Coins)+" coins")
       return
-  
+    else:
+      print("Sorry, that was not a available input")
+      print()
