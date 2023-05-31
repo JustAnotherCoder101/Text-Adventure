ad381058749c1b11f5d0e98d18b08af84fa102e5
JustAnotherCoder <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-05-31 13:55:16 +0100

    Update function.py

--- function.py
+++ function.py
@@ -1,10 +1,12 @@
 def tshop(Coins):
+  global Inventory
   print("You reach the blacksmith's")
   print("BlackSmith:Sorry Kid,this sword is the only thing available today")
   print("You Great!")
   input()
   while True:
     if input("press 1 to buy the sword for 5 coins.")==1:
+      Inventory.append("Sword")
       Coins =- 5
       print("Sword bought!")
       print("you now have "+str(Coins)+" coins")
@@ -13,18 +15,20 @@ def tshop(Coins):
       print("Sorry, that was not a available input")
       print()
       
-def tshop(Coins):
+def shop(Coins):
   input()
   while True:
     print("to buy an item press press the number")
     print("1=sword(costs 5 coins) 2=health potion(costs 2 coins)")
     input()=id
     if id ==1:
+      Inventory.append("SwordLv2")
       Coins =- 5
       print("Sword bought!")
       print("you now have "+str(Coins)+" coins")
       return -5
     elif id ==2:
+      Inventory.append("HPlv1")
       Coins =- 2
       print("Sword bought!")
       print("you now have "+str(Coins)+" coins")
